629887fe4b033467dd30de7687dd62b955d229a9
KamalRanaweera <ranaweer@ualberta.ca>
2024-08-05 21:14:55 -0600

    Initial commit

---
+++ migrations/V001_20240802185422__Kamal.sql
@@ -0,0 +1,16 @@
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Movies] (
+    [Id] int NOT NULL IDENTITY,
+    [Tittle] nvarchar(max) NOT NULL,
+    [Genre] nvarchar(max) NOT NULL,
+    [ReleaseYear] int NOT NULL,
+    [Director] nvarchar(max) NOT NULL,
+    [PictureURL] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Movies] PRIMARY KEY ([Id])
+);
+GO
+
+COMMIT;
+GO
